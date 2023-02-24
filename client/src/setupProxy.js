de6bdd33ce2db9b0006f1902b5b1ca85232d9897
rrsart2023 <123260882+rrsart2023@users.noreply.github.com>
2023-02-24 08:36:42 +0100

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require('http-proxy-middleware');
 
 
 module.exports = function(app) {
-    app.use(proxy('/api', { target: 'https://rad-skowak.onrender.com' }));
+    app.use(proxy('/api', { target: 'https://rad-skowak.onrender.com/' }));
 
 };
