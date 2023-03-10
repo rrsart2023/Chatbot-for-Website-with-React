065d5695087f871f17954eaa1cf101c2bec33565
rrsart2023 <123260882+rrsart2023@users.noreply.github.com>
2023-03-10 03:22:43 +0100

    Update chatbot.js

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,7 +1,7 @@
 'use strict';
 const dialogflow = require('dialogflow');
 const structjson = require('./structjson.js');
-const config = require('../secret.json');
+const config = require('./key.json');
 const mongoose = require('mongoose');
 
 const googleAuth = require('google-oauth-jwt');
